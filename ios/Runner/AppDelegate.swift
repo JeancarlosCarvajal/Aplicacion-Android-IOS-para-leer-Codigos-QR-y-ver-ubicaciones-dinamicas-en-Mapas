928b34c10e0d1260b06c8daef3df1027363c0b37
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-07-02 16:25:35 -0400

    Instalado Google Mpas Api Key listo para probarlo

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+// import GoogleMaps // esto proviene del paquete de google maps.... https://pub.dev/packages/google_maps_flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // GMSServices.provideAPIKey("YOUR KEY HERE") // esto proviene del PAquete con el api key de google maps.... https://pub.dev/packages/google_maps_flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/pages/mapa_page.dart
+++ lib/pages/mapa_page.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/material.dart';
-import 'package:qr_reader/models/scan_model.dart';
+import 'dart:async'; // para poder usar el Completer
 
-class MapaPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+// import 'package:qr_reader/models/scan_model.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart'; // del paquete de google maps 
+
+import 'package:qr_reader/providers/db_provider.dart';
+
+class MapaPage extends StatefulWidget {
    
   const MapaPage({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapaPage> createState() => _MapaPageState();
+}
+
+class _MapaPageState extends State<MapaPage> {
+
+  static const CameraPosition puntoInicial = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+  // sirve para trabar con el hasta que tengamos el google maps
+  final Completer<GoogleMapController> _controller = Completer(); // paquete para el Completer 'dart:async'
+   
   @override
   Widget build(BuildContext context) {
 
@@ -16,8 +34,12 @@ class MapaPage extends StatelessWidget {
       appBar: AppBar(
         title: const Center(child: Text('Mapa')),
       ),
-      body: Center(
-         child: Text((scan!.valor)),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: puntoInicial,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
